fef4abd961d643a25efdae506adb744fce111b09
swcollard <swcollard12@gmail.com>
2013-11-30 11:34:56 -0800

    Where did the tabs come from??

--- StudentEngineersCouncil/src/mobile/SEC/CompanySearchActivity.java
+++ StudentEngineersCouncil/src/mobile/SEC/CompanySearchActivity.java
@@ -22,47 +22,48 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
 public class CompanySearchActivity extends Activity {
-	
-	private Context searchContext;
-	private static Map<String,Integer> majorMap;
-	static {
-		majorMap = new HashMap<String, Integer>(23);
-    	majorMap.put("-", -1);
-    	majorMap.put("Aerospace Engineering",1);
-    	majorMap.put("Agricultural Engineering",2);
-    	majorMap.put("Biological and Agricultural Engineering",24);
-    	majorMap.put("Biomedical Engineering",4);
-    	majorMap.put("Chemical Engineering",5);
-    	majorMap.put("Civil Engineering",6);
-    	majorMap.put("Computer Engineering",7);
-    	majorMap.put("Computer Science",8);
-    	majorMap.put("Electrical Engineering",9);
-    	majorMap.put("Engineering Systems Management",11);
-    	majorMap.put("Engineering Technology",12);
-    	majorMap.put("Health Physics",13);
-    	majorMap.put("Industrial and Systems Engineering",15);
-    	majorMap.put("Industrial Distribution",14);
-    	majorMap.put("Interdisciplinary Engineering",17);
-    	majorMap.put("Materials Science and Engineering",16);
-    	majorMap.put("Mechanical Engineering",18);
-    	majorMap.put("Nuclear Engineering",19);
-    	majorMap.put("Ocean Engineering",20);
-    	majorMap.put("Petroleum Engineering",21);
-    	majorMap.put("Radiological Health Engineering",22);
-    	majorMap.put("Safety Engineering",23);
-	}
-	/** Called when the activity is first created. */
+
+    private Context searchContext;
+    private static Map<String,Integer> majorMap;
+    static {
+        majorMap = new HashMap<String, Integer>(23);
+        majorMap.put("-", -1);
+        majorMap.put("Aerospace Engineering",1);
+        majorMap.put("Agricultural Engineering",2);
+        majorMap.put("Biological and Agricultural Engineering",24);
+        majorMap.put("Biomedical Engineering",4);
+        majorMap.put("Chemical Engineering",5);
+        majorMap.put("Civil Engineering",6);
+        majorMap.put("Computer Engineering",7);
+        majorMap.put("Computer Science",8);
+        majorMap.put("Electrical Engineering",9);
+        majorMap.put("Engineering Systems Management",11);
+        majorMap.put("Engineering Technology",12);
+        majorMap.put("Health Physics",13);
+        majorMap.put("Industrial and Systems Engineering",15);
+        majorMap.put("Industrial Distribution",14);
+        majorMap.put("Interdisciplinary Engineering",17);
+        majorMap.put("Materials Science and Engineering",16);
+        majorMap.put("Mechanical Engineering",18);
+        majorMap.put("Nuclear Engineering",19);
+        majorMap.put("Ocean Engineering",20);
+        majorMap.put("Petroleum Engineering",21);
+        majorMap.put("Radiological Health Engineering",22);
+        majorMap.put("Safety Engineering",23);
+    }
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search); 
-        
+
         searchContext = this;
         // Days Attending Spinner
         Spinner daySpinner = (Spinner) findViewById(R.id.daySpinner);
@@ -71,7 +72,7 @@ public class CompanySearchActivity extends Activity {
         dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         daySpinner.setAdapter(dayAdapter);
         
-    	// majorMap Spinner
+        // majorMap Spinner
         Spinner majorMappinner = (Spinner) findViewById(R.id.majorSpinner);
         ArrayAdapter<CharSequence> majorAdapter = ArrayAdapter.createFromResource(
                 this, R.array.MajorsArray, android.R.layout.simple_spinner_item);
@@ -91,23 +92,25 @@ public class CompanySearchActivity extends Activity {
                 this, R.array.EmploymentArray, android.R.layout.simple_spinner_item);
         employmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         employmentSpinner.setAdapter(employmentAdapter);
-
+        
+        Button searchButton = (Button) findViewById(R.id.searchButton);
+        searchButton.requestFocus();
     }
-    
-	/** public void onSearchClick(View view)
-	 * 	Executes a search
-	 * @param view	
-	 */
+
+    /**
+     *  Executes a search
+     * @param view	
+     */
     public void onSearchClick(View view) {
     	String params = getParams();
         String url = "http://sec.tamu.edu/Students/CareerFair/MobileSearch.aspx" + params;
         new Search().execute(url);
     }
-    
-    /** private String getParams()
+
+    /**
      * gets values from UI elements to include in search request
      * Java 1.6 does not support switch statements for String objects
-     * @return a String of parameters for the 
+     * @return a String of parameters for the search
      */
     private String getParams() {
     	String query = ((EditText)findViewById(R.id.queryText)).getText().toString();
